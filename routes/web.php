2eb995755b4cfdf6d62ec1b608a80cfd477cddf0
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2024-08-29 21:00:46 +0800

    removed .env in .gitignore

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
     return view('index');
 })->name('homepage');
 
